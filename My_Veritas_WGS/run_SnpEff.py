977dfe74176687c9949b5a974642496db4a1c956
Charles Warden <cwarden45@gmail.com>
2016-08-04 23:42:40 -0700

    Add files via upload

--- My_Veritas_WGS/run_SnpEff.py
+++ My_Veritas_WGS/run_SnpEff.py
@@ -36,11 +36,16 @@ for arg in sys.argv:
 		sys.exit()
 
 vcfCheck = re.search(".vcf$",input)
+bedCheck = re.search(".bed$",input)
 		
 if caller == "Veritas":
 	if (not vcfCheck):
 		print "You must provide a vcf --input file"
 		sys.exit()
+elif (caller == "DELLY") or (caller == "LUMPY"):
+	if (not bedCheck):
+		print "You must provide a vcf --input file"
+		sys.exit()		
 else:
 	print "Need to provide valid --caller and --input combination"
 	sys.exit()
@@ -96,7 +101,79 @@ if caller == "Veritas":
 	command = "java -jar -Xmx" + javaMem + " /opt/snpEff/snpEff.jar " + refDb + " " + tempVcf + " > " + snpEffAnn
 	os.system(command)
 	
+	outHandle.close()
+	
 	command = "rm "+ tempVcf
 	os.system(command)
+elif (caller == "DELLY") or (caller == "LUMPY"):
+	annotatedBED = re.sub(".bed$","_snpEff_genes.bed",input)
+
+	tempBED = "temp.bed"
+	outHandle = open(tempBED, 'w')
+	
+	inHandle = open(input)
+	line = inHandle.readline()
+			
+	while line:
+		line = re.sub("\n","",line)
+		line = re.sub("\r","",line)
+
+		commentFlag = re.search("^#",line)
+		
+		if commentFlag:
+			text = line + "\n"
+			outHandle.write(text)
+		else:
+			lineInfo = line.split("\t")
+			lineInfo[0] = re.sub("chr","",lineInfo[0])
+			if lineInfo[0] == "M":
+				lineInfo[0] = "MT"
+			text = "\t".join(lineInfo) + "\n"
+			outHandle.write(text)
+		line = inHandle.readline()	
+	
+	outHandle.close()
+	inHandle.close()
+	
+	print "Running SnpEff..."
+	command = "java -jar -Xmx" + javaMem + " /opt/snpEff/snpEff.jar -i bed " + refDb + " " + tempBED + " > " + annotatedBED
+	os.system(command)
+	
+	command = "rm "+ tempBED
+	os.system(command)
+	
+	print "Filtering for exonic variants"
+	exonBED = re.sub(".bed$","_snpEff_genes_EXONIC.bed",input)
+	
+	outHandle = open(exonBED, 'w')
+	
+	inHandle = open(annotatedBED)
+	line = inHandle.readline()
+			
+	while line:
+		line = re.sub("\n","",line)
+		line = re.sub("\r","",line)
+
+		commentFlag = re.search("^#",line)
+		
+		if commentFlag:
+			text = line + "\n"
+			outHandle.write(text)
+		else:
+			lineInfo = line.split("\t")
+			chr = "chr" + lineInfo[0]
+			start = lineInfo[1]
+			stop = lineInfo[2]
+			annText = lineInfo[3]
+			annInfo = annText.split(";")
+			for ann in annInfo:
+				exonResult = re.search("Exon",ann)
+				if exonResult:
+					pseudoResult = re.search("pseudogene",ann)
+					antisenseResult = re.search("antisense",ann)
+					if (not pseudoResult) and (not antisenseResult):
+						text = chr + "\t" + start + "\t" + stop + "\t" + ann + "\n"
+						outHandle.write(text)
+		line = inHandle.readline()	
 else:
-	print "Template currently only written for Veritas / VCF annoations"
+	print "Code currently only valid for Veritas VCF and BED annotations"
