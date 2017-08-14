01120bcd02ff8448e5ec1def304607de786a4591
Charles Warden <cwarden45@gmail.com>
2017-08-14 13:36:29 -0700

    Add files via upload

--- Exome_Workflow/annotate_variants_varscan_somatic.py
+++ Exome_Workflow/annotate_variants_varscan_somatic.py
@@ -47,7 +47,7 @@ if (annovarPath == "") or (annovarPath == "[required]"):
 	sys.exit()
 
 statHandle = open(countFile,"w")
-text = "pairID\tSNPcount\tDel.Count\tIns.Count\n"
+text = "pairID\tSomatic.SNP.Count\tSomatic.Del.Count\tSomatic.Ins.Count\tLOH.SNP.Count\tLOH.Del.Count\tLOH.Ins.Count\n"
 statHandle.write(text)
 
 
@@ -66,140 +66,150 @@ for line in lines:
 		lineInfo = line.split("\t")
 		varscanPrefix = lineInfo[0]	
 		print varscanPrefix
+		
+		if(varscanPrefix not in finishedSamples):
+			resultSubfolder = resultFolder + "/" + varscanPrefix
+			command = "mkdir " + resultSubfolder
+			os.system(command)
+			
+			varScanSnp = varscanPrefix + ".snp"
+			varScanIndel = varscanPrefix + ".indel"
+			snpCountSomatic = 0
+			insCountSomatic = 0
+			delCountSomatic = 0
+			snpCountLOH = 0
+			insCountLOH = 0
+			delCountLOH = 0
+			
+			annovarVar = resultSubfolder + "/" + varscanPrefix + ".avinput"
+			outHandle = open(annovarVar,"w")
+			#chr	start	stop	ref	var	status	qual(p-value)	cov
 
-		resultSubfolder = resultFolder + "/" + varscanPrefix
-		command = "mkdir " + resultSubfolder
-		os.system(command)
-		
-		varScanSnp = varscanPrefix + ".snp"
-		varScanIndel = varscanPrefix + ".indel"
-		snpCount = 0
-		insCount = 0
-		delCount = 0
-				
-		annovarVar = resultSubfolder + "/" + varscanPrefix + ".avinput"
-		outHandle = open(annovarVar,"w")
-		#chr	start	stop	ref	var	status	qual(p-value)	cov
-
-		#convert SNP file
-		inHandle2 = open(varScanSnp)
-		line2 = inHandle2.readline()
-		
-		lineCount2 = 0
-		
-		while line2:
-			lineCount2 += 1
-			if lineCount2 > 1:
-				lineInfo = line2.split("\t")
-				chr = lineInfo[0]
-				pos = lineInfo[1]
-				ref = lineInfo[2]
-				var = lineInfo[3]
-				status = lineInfo[12]
-				somatic_pvalue = lineInfo[14]
-				tumorRefCov = int(lineInfo[8])
-				tumorVarCov = int(lineInfo[9])
-				totalCov = tumorRefCov + tumorVarCov
-				
-				if (len(ref) == 1 or len(var) == 1) and (status != "Germline"):
-					if status == "Somatic":
-						snpCount += 1
-					#skip variants with multiple variant alleles --> separate frequencies aren't provided anyways
-					text = chr + "\t" + pos+ "\t" + pos + "\t" + ref + "\t" + var + "\t" + status + "\tp=" + somatic_pvalue + "\t" + str(totalCov) + "\n"
-					outHandle.write(text)
-					
+			#convert SNP file
+			inHandle2 = open(varScanSnp)
 			line2 = inHandle2.readline()
-		inHandle2.close()
-
-		#convert indel file
-		inHandle2 = open(varScanIndel)
-		line2 = inHandle2.readline()
-		
-		lineCount2 = 0
-		
-		while line2:
-			lineCount2 += 1
-			if lineCount2 > 1:
-				lineInfo = line2.split("\t")
-				chr = lineInfo[0]
-				pos = int(lineInfo[1])
-				ref = lineInfo[2]
-				var = lineInfo[3]
-				status = lineInfo[12]
-				somatic_pvalue = lineInfo[14]
-				tumorRefCov = int(lineInfo[8])
-				tumorVarCov = int(lineInfo[9])
-				totalCov = tumorRefCov + tumorVarCov
-				
-				insFlag = re.search("^\+([ACGT]+)",var)
-				delFlag = re.search("^-([ACGT]+)",var)
-				
-				if insFlag and (status != "Germline"):
-					if status == "Somatic":
-						insCount += 1
+			
+			lineCount2 = 0
+			
+			while line2:
+				lineCount2 += 1
+				if lineCount2 > 1:
+					lineInfo = line2.split("\t")
+					chr = lineInfo[0]
+					pos = lineInfo[1]
+					ref = lineInfo[2]
+					var = lineInfo[3]
+					status = lineInfo[12]
+					somatic_pvalue = lineInfo[14]
+					tumorRefCov = int(lineInfo[8])
+					tumorVarCov = int(lineInfo[9])
+					totalCov = tumorRefCov + tumorVarCov
+					
+					if (len(ref) == 1 or len(var) == 1) and (status != "Germline"):
+						if status == "Somatic":
+							snpCountSomatic += 1
+						if status == "LOH":
+							snpCountLOH += 1
+						#skip variants with multiple variant alleles --> separate frequencies aren't provided anyways
+						text = chr + "\t" + pos+ "\t" + pos + "\t" + ref + "\t" + var + "\t" + status + "\tp=" + somatic_pvalue + "\t" + str(totalCov) + "\n"
+						outHandle.write(text)
 						
-					var = insFlag.group(1)
-					text = chr + "\t" + str(pos)+ "\t" + str(pos+len(var)-1) + "\t-\t" + var + "\t" + status + "\tp=" + somatic_pvalue + "\t" + str(totalCov) + "\n"
-					outHandle.write(text)
+				line2 = inHandle2.readline()
+			inHandle2.close()
 
-				if delFlag and (status != "Germline"):
-					if status == "Somatic":
-						delCount += 1
-					var = delFlag.group(1)
-					text = chr + "\t" + str(pos+1)+ "\t" + str(pos+1+len(var)-1) + "\t" + var + "\t-\t" + status + "\tp=" + somatic_pvalue + "\t" + str(totalCov) + "\n"
-					outHandle.write(text)
-					
+			#convert indel file
+			inHandle2 = open(varScanIndel)
 			line2 = inHandle2.readline()
-		inHandle2.close()
-		
-		outHandle.close()
-		
-		text = varscanPrefix + "\t"+str(snpCount)+"\t"+str(delCount)+"\t"+str(insCount)+"\n"
-		statHandle.write(text)
-
-		annotationPrefix = resultSubfolder + "/" + varscanPrefix
-		if build == "hg19":
-			command = annovarPath + "table_annovar.pl --otherinfo " + annovarVar +" " + annovarPath + "humandb/ -csvout -buildver "+build+" -out " + annotationPrefix +" -protocol refGene,clinvar_20160302,cosmic70,nci60,kaviar_20150923,hrcr1,dbnsfp30a,gnomad_exome,avsnp147,cadd13gt20,gwava -operation g,f,f,f,f,f,f,f,f,f,f -nastring NA --thread " + threads
-			os.system(command)
-		else:
-			command = annovarPath + "table_annovar.pl --otherinfo " + annovarVar +" " + annovarPath + "humandb/ -csvout -buildver "+build+" -out " + annotationPrefix +" -protocol refGene,clinvar_20160302,cosmic70,nci60,kaviar_20150923,hrcr1,dbnsfp30a,gnomad_exome,avsnp147 -operation g,f,f,f,f,f,f,f,f -nastring NA --thread " + threads
-			os.system(command)
-
-		bedAnn = build + "_GWAScatalog.bed"
-		annotationPrefix = resultSubfolder + "/" + varscanPrefix + "_annovar_GWAS_Catalog"
-		command = annovarPath + "annotate_variation.pl " + annovarVar +" " + annovarPath + "humandb/ -buildver "+build+" -out " + annotationPrefix +" -bedfile " + bedAnn + " -dbtype bed -regionanno -colsWanted 4"
-		os.system(command)
-
-		#bed annotation mostly works OK, but bedtools was more accurate for ORegAnno hit (probably because it isn't sorted)
-		bedAnn = annovarPath + "/humandb/" + build + "_ORegAnno.bed"
-		bedOut = resultSubfolder + "/" + varscanPrefix + "_bedtools_ORegAnno.bed"
-		command = "/opt/bedtools2/bin/bedtools intersect -wa -wb -a " + annovarVar +" -b " + bedAnn + " > " + bedOut
-		os.system(command)
-		
-		#minor formatting modification to work with variant summary script
-		oregannoANNOVAR= resultSubfolder + "/" + varscanPrefix + "_bedtools_ORegAnno.avinput"
-		outHandle = open(oregannoANNOVAR,"w")
-		
-		inHandle2 = open(bedOut)
-		line2 = inHandle2.readline()
-		
-		while line2:
-			lineInfo = line2.split("\t")
-			if(len(lineInfo) > 11):
-				chr = lineInfo[0]
-				start = lineInfo[1]
-				stop = lineInfo[2]
-				ref = lineInfo[3]
-				var = lineInfo[4]
-				type = lineInfo[5]
-				somatic_pvalue = lineInfo[6]
-				totalCov = lineInfo[7]
-				oreg = lineInfo[11]
+			
+			lineCount2 = 0
+			
+			while line2:
+				lineCount2 += 1
+				if lineCount2 > 1:
+					lineInfo = line2.split("\t")
+					chr = lineInfo[0]
+					pos = int(lineInfo[1])
+					ref = lineInfo[2]
+					var = lineInfo[3]
+					status = lineInfo[12]
+					somatic_pvalue = lineInfo[14]
+					tumorRefCov = int(lineInfo[8])
+					tumorVarCov = int(lineInfo[9])
+					totalCov = tumorRefCov + tumorVarCov
 					
-				text = chr + "\t" + start+ "\t" + stop + "\t" + ref + "\t" + var + "\t" + type + "\t" + somatic_pvalue+ "\t" + oreg + "\n"
-				outHandle.write(text)
+					insFlag = re.search("^\+([ACGT]+)",var)
+					delFlag = re.search("^-([ACGT]+)",var)
 					
+					if insFlag and (status != "Germline"):
+						if status == "Somatic":
+							insCountSomatic += 1
+						if status == "LOH":
+							insCountLOH += 1						
+						var = insFlag.group(1)
+						text = chr + "\t" + str(pos)+ "\t" + str(pos+len(var)-1) + "\t-\t" + var + "\t" + status + "\tp=" + somatic_pvalue + "\t" + str(totalCov) + "\n"
+						outHandle.write(text)
+
+					if delFlag and (status != "Germline"):
+						if status == "Somatic":
+							delCountSomatic += 1
+						if status == "LOH":
+							delCountLOH += 1	
+							
+						var = delFlag.group(1)
+						text = chr + "\t" + str(pos+1)+ "\t" + str(pos+1+len(var)-1) + "\t" + var + "\t-\t" + status + "\tp=" + somatic_pvalue + "\t" + str(totalCov) + "\n"
+						outHandle.write(text)
+						
+				line2 = inHandle2.readline()
+			inHandle2.close()
+			
+			outHandle.close()
+			
+			text = varscanPrefix + "\t"+str(snpCountSomatic)+"\t"+str(delCountSomatic)+"\t"+str(insCountSomatic)+ "\t"+str(snpCountLOH)+"\t"+str(delCountLOH)+"\t"+str(insCountLOH)+"\n"
+			statHandle.write(text)
+
+			annotationPrefix = resultSubfolder + "/" + varscanPrefix
+			if build == "hg19":
+				command = annovarPath + "table_annovar.pl --otherinfo " + annovarVar +" " + annovarPath + "humandb/ -csvout -buildver "+build+" -out " + annotationPrefix +" -protocol refGene,clinvar_20160302,cosmic70,nci60,kaviar_20150923,hrcr1,dbnsfp30a,gnomad_exome,avsnp147,cadd13gt20,gwava -operation g,f,f,f,f,f,f,f,f,f,f -nastring NA --thread " + threads
+				os.system(command)
+			else:
+				command = annovarPath + "table_annovar.pl --otherinfo " + annovarVar +" " + annovarPath + "humandb/ -csvout -buildver "+build+" -out " + annotationPrefix +" -protocol refGene,clinvar_20160302,cosmic70,nci60,kaviar_20150923,hrcr1,dbnsfp30a,gnomad_exome,avsnp147 -operation g,f,f,f,f,f,f,f,f -nastring NA --thread " + threads
+				os.system(command)
+
+			bedAnn = build + "_GWAScatalog.bed"
+			annotationPrefix = resultSubfolder + "/" + varscanPrefix + "_annovar_GWAS_Catalog"
+			command = annovarPath + "annotate_variation.pl " + annovarVar +" " + annovarPath + "humandb/ -buildver "+build+" -out " + annotationPrefix +" -bedfile " + bedAnn + " -dbtype bed -regionanno -colsWanted 4"
+			os.system(command)
+
+			#bed annotation mostly works OK, but bedtools was more accurate for ORegAnno hit (probably because it isn't sorted)
+			bedAnn = annovarPath + "/humandb/" + build + "_ORegAnno.bed"
+			bedOut = resultSubfolder + "/" + varscanPrefix + "_bedtools_ORegAnno.bed"
+			command = "/opt/bedtools2/bin/bedtools intersect -wa -wb -a " + annovarVar +" -b " + bedAnn + " > " + bedOut
+			os.system(command)
+			
+			#minor formatting modification to work with variant summary script
+			oregannoANNOVAR= resultSubfolder + "/" + varscanPrefix + "_bedtools_ORegAnno.avinput"
+			outHandle = open(oregannoANNOVAR,"w")
+			
+			inHandle2 = open(bedOut)
 			line2 = inHandle2.readline()
-		inHandle2.close()
-		outHandle.close()
-	inHandle.close()
+			
+			while line2:
+				lineInfo = line2.split("\t")
+				if(len(lineInfo) > 11):
+					chr = lineInfo[0]
+					start = lineInfo[1]
+					stop = lineInfo[2]
+					ref = lineInfo[3]
+					var = lineInfo[4]
+					type = lineInfo[5]
+					somatic_pvalue = lineInfo[6]
+					totalCov = lineInfo[7]
+					oreg = lineInfo[11]
+						
+					text = chr + "\t" + start+ "\t" + stop + "\t" + ref + "\t" + var + "\t" + type + "\t" + somatic_pvalue+ "\t" + oreg + "\n"
+					outHandle.write(text)
+						
+				line2 = inHandle2.readline()
+			inHandle2.close()
+			outHandle.close()
+		inHandle.close()
