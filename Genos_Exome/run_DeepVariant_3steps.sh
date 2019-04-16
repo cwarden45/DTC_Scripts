d15c2c2859ccb96763fdc6da61117fecff12f6dd
Charles Warden <cwarden45@gmail.com>
2019-04-15 19:52:07 -0700

    Update run_DeepVariant_3steps.sh

--- Genos_Exome/run_DeepVariant_3steps.sh
+++ Genos_Exome/run_DeepVariant_3steps.sh
@@ -3,7 +3,7 @@
 #something very similar to this worked on AWS (although that requires "yum" instead of "apt-get")
 
 N_SHARDS=8
-OUTPUT_DIR=/mnt/cdw-genome/Charles_Human/Genos_Exome/BWA-MEM_Alignment/Genos_BWA-MEM
+OUTPUT_DIR=/mnt/cdw-genome/Charles_Human/Genos_Exome/BWA-MEM_Alignment/DeepVariant_Output
 REF=/mnt/cdw-genome/Ref/hg19.gatk.fasta
 BAM=/mnt/cdw-genome/Charles_Human/Genos_Exome/BWA-MEM_Alignment/BWA-MEM_realign_TARGET.bam
 MODEL_NAME=DeepVariant-inception_v3-0.7.2+data-wes_standard
@@ -17,15 +17,15 @@ LOGDIR=logs
 
 ## step #1: make_examples ##
 
-sudo apt-get -y install time
+sudo apt-get install time
 sudo apt-get -y install parallel #you may have to install this before running the script
 
-mkdir -p ${OUTPUT_DIR}
-mkdir -p "${LOGDIR}"
+sudo mkdir -p ${OUTPUT_DIR}
+sudo mkdir -p "${LOGDIR}"
 
 time seq 0 $((N_SHARDS-1)) | \
   parallel --eta --halt 2 --joblog "${LOGDIR}/log" --res "${LOGDIR}" \
-  docker run \
+  sudo docker run \
     -v /home/cwarden/cdw-genome:/mnt/cdw-genome \
     gcr.io/deepvariant-docker/deepvariant \
     /opt/deepvariant/bin/make_examples \
@@ -37,7 +37,7 @@ time seq 0 $((N_SHARDS-1)) | \
 
 ## step #2: call_variants ##
 
-docker run \
+sudo docker run \
   -v /home/cwarden/cdw-genome:/mnt/cdw-genome \
   gcr.io/deepvariant-docker/deepvariant \
   /opt/deepvariant/bin/call_variants \
@@ -47,7 +47,7 @@ docker run \
  
 ## step #3: postprocess_variants ##
 
-docker run \
+sudo docker run \
   -v /home/cwarden/cdw-genome:/mnt/cdw-genome \
   gcr.io/deepvariant-docker/deepvariant \
   /opt/deepvariant/bin/postprocess_variants \
