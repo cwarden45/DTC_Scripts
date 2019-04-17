80bb9df0a181a3c0181e59c6691c060f0b7e11cb
Charles Warden <cwarden45@gmail.com>
2019-04-16 19:00:24 -0700

    Add files via upload

---
+++ Genos_Exome/run_DeepVariant_gcp_deepvariant_runner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+# Set common settings.
+PROJECT_ID=cdw-deepvariant-wgs-exome
+OUTPUT_BUCKET=gs://cdw-genome/Charles_Human/Genos_Exome/BWA-MEM_Alignment/DeepVariant_Output
+STAGING_FOLDER_NAME=wes_staging
+OUTPUT_FILE_NAME=Exome_BWA-MEM.vcf
+
+## Model for calling Exome sequencing data
+MODEL=gs://deepvariant/models/DeepVariant/0.7.2/DeepVariant-inception_v3-0.7.2+data-wes_standard
+
+IMAGE_VERSION=0.7.2
+DOCKER_IMAGE=gcr.io/deepvariant-docker/deepvariant:"${IMAGE_VERSION}"
+COMMAND="/opt/deepvariant_runner/bin/gcp_deepvariant_runner \
+  --project ${PROJECT_ID} \
+  --zones us-west2-* \
+  --docker_image ${DOCKER_IMAGE} \
+  --outfile ${OUTPUT_BUCKET}/${OUTPUT_FILE_NAME} \
+  --staging ${OUTPUT_BUCKET}/${STAGING_FOLDER_NAME} \
+  --model ${MODEL} \
+  --bam gs://cdw-genome/Charles_Human/Genos_Exome/BWA-MEM_Alignment/BWA-MEM_realign_TARGET.bam \
+  --ref gs://cdw-genome/Ref/hg19.gatk.fasta \
+  --gcsfuse"
+  
+# Run the pipeline.
+gcloud alpha genomics pipelines run \
+    --project "${PROJECT_ID}" \
+    --service-account-scopes="https://www.googleapis.com/auth/cloud-platform" \
+    --logging "${OUTPUT_BUCKET}/${STAGING_FOLDER_NAME}/runner_logs_$(date +%Y%m%d_%H%M%S).log" \
+	--docker-image gcr.io/deepvariant-docker/deepvariant_runner:"${IMAGE_VERSION}" \
+    --command-line "${COMMAND}"
