b791617efc4c5a0d2845110d76346ae8198b6dab
Charles Warden <cwarden45@gmail.com>
2019-04-19 17:33:06 -0700

    Add files via upload

---
+++ Genos_Exome/convert_to_precisionFDA.pl
@@ -0,0 +1,76 @@
+use warnings;
+use strict;
+use diagnostics;
+
+#I compress file afterwards (so, don't put ".gz" in the output file name)
+
+#my $inputVCF = "82651510240740_annotated.vcf";
+#my $outputVCF = "82651510240740_annotated_precisionFDA.vcf";
+
+my $inputVCF = "K33YDXX.vcf";
+my $outputVCF = "K33YDXX_precisionFDA.vcf";
+
+print "Reformatting VCF\n";
+
+open(OUTPUTFILE, "> $outputVCF") || die("Could not open $outputVCF!");
+	
+open(INPUTFILE, $inputVCF) || die("Could not open $inputVCF!");
+while (<INPUTFILE>){
+	my $line = $_;
+	chomp $line;
+	$line =~ s/\r//g;
+	$line =~ s/\n//g;
+	
+	my @lineInfo = split("\t",$line);
+	
+	if ($line =~ /^##/){
+		print OUTPUTFILE "$line\n";
+	}elsif ($line =~ /^#/){
+		#Veritas WGS incorrectly had an extra sample name in the header (so, remove that)
+		while(scalar(@lineInfo) > 10){
+			pop(@lineInfo);
+		}#end while(scalar(@lineInfo) > 10)
+		print OUTPUTFILE join("\t",@lineInfo),"\n";
+	}else{
+		my $chr = $lineInfo[0];
+		my $pos = $lineInfo[1];
+		my $varID = $lineInfo[2];
+		my $ref = uc($lineInfo[3]);
+		my $var = uc($lineInfo[4]);
+		my $flag = $lineInfo[6];
+		my $format_text = $lineInfo[8];
+		my $geno_text = $lineInfo[9];
+		
+		if($flag eq "PASS"){
+			#Genos provided .vcf with some other rows
+			#hopefully, this works with .vcf files from all programs: otherwise, perhaps need to modify code
+			
+			if($chr eq "chrM"){
+				$chr = "MT";
+			}#end if($chr eq "chrM")
+			if ($chr =~ /^chr/){
+				$chr =~ s/^chr//;
+			}#end if ($chr =~ /^chr/)
+			$lineInfo[0] =  $chr;
+
+			#delete if you can't actually have complex variants
+			print OUTPUTFILE join("\t",@lineInfo),"\n";
+
+			#if (!($var =~ /,/)){
+			#	#I think I need to remove complex variants - I got an error message when I tried to use https://platform.dnanexus.com/login?scope=%7B%22full%22%3A+true%7D&redirect_uri=https%3A%2F%2Fprecision.fda.gov%2Freturn_from_login&client_id=precision_fda_gov (with .bed file for RefSeq CDS regions)
+			#	print OUTPUTFILE join("\t",@lineInfo),"\n";
+			#}else{
+			#	print "Filter complex variant -  ref: $ref, var: $var\n";
+			#}#end else
+
+		}#end if($flag eq "PASS")
+	}#end if (!($line =~ /^#/))
+}#end while (<INPUTFILE>)
+close(INPUTFILE);
+close(OUTPUTFILE);
+
+print "Compressing VCF\n";
+my $command = "gzip $outputVCF";
+system($command);
+
+exit;
