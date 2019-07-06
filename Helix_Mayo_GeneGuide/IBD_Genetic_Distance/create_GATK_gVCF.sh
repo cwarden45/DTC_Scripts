d5ef5730f542120fcc4997848a8146bad05c3831
Charles Warden <cwarden45@gmail.com>
2019-07-06 14:37:10 -0700

    Add files via upload

--- Helix_Mayo_GeneGuide/IBD_Genetic_Distance/create_GATK_gVCF.sh
+++ Helix_Mayo_GeneGuide/IBD_Genetic_Distance/create_GATK_gVCF.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-BAM=82651510240740.mapped.sorted.markdup.realn.recal.bam
-gVCF=82651510240740.mapped.sorted.markdup.realn.recal.gVCF
+BAM=hg19.gatk.bam
+gVCF=hg19.gatk.gVCF
+gVCF2=hg19.gatk.flagged.gVCF
 REF=../hg19.gatk.fasta
 
 /opt/gatk-4.1.2.0/gatk --java-options -Xmx6g HaplotypeCaller --input $BAM --reference $REF --output $gVCF --dont-use-soft-clipped-bases true --emit-ref-confidence GVCF
+/opt/gatk-4.1.2.0/gatk --java-options -Xmx6g VariantFiltration --variant $gVCF --output $gVCF2 -window 35 -cluster 3 -filter-name QD -filter "QD < 2.0" -filter-name FS -filter "FS > 30.0"
