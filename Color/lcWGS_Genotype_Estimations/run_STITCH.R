0f67018d3d671372e5e33c60bed273e7bcb00459
Charles Warden <cwarden45@gmail.com>
2020-03-18 09:30:19 -0700

    Add files via upload

--- Color/lcWGS_Genotype_Estimations/run_STITCH.R
+++ Color/lcWGS_Genotype_Estimations/run_STITCH.R
@@ -1,14 +1,16 @@
-sampleID = "Nebula_full_nodup"
-
-### Hopefully, edit above this point?
+bam_list = "Nebula_full_nodup-bams.txt"
+name_list = "Nebula_full_nodup-names.txt"
+output_prefix = "Nebula_full_nodup"
+pos_folder = "human_g1k_v37-pos_files"
 
 library("STITCH")
 
 #human_genfile = "gen_sequencing.txt" #this is supposed to be optional
 human_K = 10
 human_nGen = 4 * 20000 / human_K
+temp_folder = tempdir()
 
-human_posfile = "STITCH_human_example_2016_10_18/pos-ALT.txt"
+human_posfile = "STITCH_human_example_2016_10_18/pos.txt"
 human_reference_sample_file = "1000GP_Phase3.sample"
 human_reference_legend_file =  "1000GP_Phase3_chr20.legend.gz"
 human_reference_haplotype_file = "1000GP_Phase3_chr20.hap.gz"
@@ -21,18 +23,21 @@ human_matched_to_reference_reference_haplotype_file = "STITCH_human_reference_ex
 
 #try to follow human example 3: https://github.com/rwdavies/STITCH/blob/master/examples/examples.R
 
+chr = 20
+output_folder = paste(output_prefix,"_",chr,sep="")
+
 STITCH(
-  bamlist = paste(sampleID,"-bams.txt",sep=""),#I think this is supposed to be file with a list of .bams, not 1 bam file (or a "list" of .bam files)
-  sampleNames_file = paste(sampleID,"-names.txt",sep=""),
-  outputdir = sampleID,
+  bamlist = bam_list,
+  sampleNames_file = name_list,
+  outputdir = output_folder,
   method = "diploid",
   originalRegionName = "20.1000000.1100000",
   regenerateInput = TRUE,
   regionStart = 1000000,
   regionEnd = 1100000,
   buffer = 10000,
-  niterations = 1,
-  chr = "chr20",
+  niterations = 20,
+  chr = chr,
   inputBundleBlockSize = 100,
   reference_populations = c("CEU", "GBR", "ACB"),
   reference_haplotype_file = human_matched_to_reference_reference_haplotype_file,
@@ -41,4 +46,4 @@ STITCH(
   posfile = human_posfile,
   shuffleHaplotypeIterations = NA,
   refillIterations = NA,
-  K = human_K, tempdir = "temp", nCores = detectCores(), nGen = human_nGen)
+  K = human_K, tempdir = temp_folder, nCores = 1, nGen = human_nGen)
