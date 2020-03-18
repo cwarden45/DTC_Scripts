71494b0c813272bd88dcfe1635a4c7863204271c
Charles Warden <cwarden45@gmail.com>
2020-03-18 14:53:17 -0700

    Add files via upload

--- Color/lcWGS_Genotype_Estimations/run_STITCH.R
+++ Color/lcWGS_Genotype_Estimations/run_STITCH.R
@@ -1,49 +1,50 @@
 bam_list = "Nebula_full_nodup-bams.txt"
 name_list = "Nebula_full_nodup-names.txt"
-output_prefix = "Nebula_full_nodup"
-pos_folder = "human_g1k_v37-pos_files"
+output_prefix = "1KG_IMPUTE2-Nebula_full_nodup"
 
 library("STITCH")
+pos_folder = "human_g1k_v37-pos_files"
+
+autosomal_chr = 1:22
+fa_index =  "../1000_Genomes_BAMs/Ref/human_g1k_v37.fasta.fai"
+fa_index.table = read.table(fa_index, head=F, sep="\t")
 
 #human_genfile = "gen_sequencing.txt" #this is supposed to be optional
 human_K = 10
 human_nGen = 4 * 20000 / human_K
 temp_folder = tempdir()
 
-human_posfile = "STITCH_human_example_2016_10_18/pos.txt"
-human_reference_sample_file = "1000GP_Phase3.sample"
-human_reference_legend_file =  "1000GP_Phase3_chr20.legend.gz"
-human_reference_haplotype_file = "1000GP_Phase3_chr20.hap.gz"
-
-human_matched_to_reference_genfile = "STITCH_human_reference_example_2018_07_11/gen_sequencing.intersect.txt"#also optional?
-human_matched_to_reference_posfile = "STITCH_human_reference_example_2018_07_11/pos.intersect.txt"
-human_matched_to_reference_reference_legend_file = "STITCH_human_reference_example_2018_07_11/1000GP_Phase3_20.1000000.1100000.legend.gz"
-human_matched_to_reference_reference_haplotype_file = "STITCH_human_reference_example_2018_07_11/1000GP_Phase3_20.1000000.1100000.hap.gz"
-
-
 #try to follow human example 3: https://github.com/rwdavies/STITCH/blob/master/examples/examples.R
 
-chr = 20
-output_folder = paste(output_prefix,"_",chr,sep="")
+for (chr in autosomal_chr){
+	print(paste("Running STITCH for ",chr,"...",sep=""))
+	human_posfile = paste(pos_folder,"/",chr,"_pos.txt",sep="")
+	output_folder = paste(output_prefix,"_",chr,sep="")
 
-STITCH(
-  bamlist = bam_list,
-  sampleNames_file = name_list,
-  outputdir = output_folder,
-  method = "diploid",
-  originalRegionName = "20.1000000.1100000",
-  regenerateInput = TRUE,
-  regionStart = 1000000,
-  regionEnd = 1100000,
-  buffer = 10000,
-  niterations = 20,
-  chr = chr,
-  inputBundleBlockSize = 100,
-  reference_populations = c("CEU", "GBR", "ACB"),
-  reference_haplotype_file = human_matched_to_reference_reference_haplotype_file,
-  reference_sample_file = human_reference_sample_file,
-  reference_legend_file = human_matched_to_reference_reference_legend_file,
-  posfile = human_posfile,
-  shuffleHaplotypeIterations = NA,
-  refillIterations = NA,
-  K = human_K, tempdir = temp_folder, nCores = 1, nGen = human_nGen)
+	human_reference_sample_file = "../1000_Genomes_BAMs/IMPUTE2_Files/1000GP_Phase3/1000GP_Phase3.sample"
+	human_reference_legend_file = paste("../1000_Genomes_BAMs/IMPUTE2_Files/1000GP_Phase3/1000GP_Phase3_chr",chr,".legend.gz",sep="")
+	human_reference_haplotype_file = paste("../1000_Genomes_BAMs/IMPUTE2_Files/1000GP_Phase3/1000GP_Phase3_chr",chr,".hap.gz",sep="")
+
+	STITCH(
+		bamlist = bam_list,
+		sampleNames_file = name_list,
+		outputdir = output_folder,
+		method = "diploid",
+		regenerateInput = TRUE,
+		regionStart = 1,
+		regionEnd = fa_index.table$V2[fa_index.table$V1 == chr],
+		buffer = 10000,
+		niterations = 20,
+		chr = chr,
+		inputBundleBlockSize = 100,
+		reference_populations = c("CEU", "GBR", "ACB"),
+		reference_haplotype_file = human_reference_haplotype_file,
+		reference_sample_file = human_reference_sample_file,
+		reference_legend_file = human_reference_legend_file,
+		posfile = human_posfile,
+		shuffleHaplotypeIterations = NA,
+		refillIterations = NA,
+		K = human_K, tempdir = temp_folder, nCores = 1, nGen = human_nGen)
+	  
+	  stop()
+}#end for (chr in autosomal_chr)
