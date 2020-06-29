1b7fbfa4c630d85e64972d4c93e1d5f48f09bc6e
Charles Warden <cwarden45@gmail.com>
2020-06-29 13:39:11 -0700

    Add files via upload

---
+++ Genos_Exome/Exome_Ancestry/On-Target/run_ADMIXTURE.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PREFIX=../../../Mayo_GeneGuide/plink_IBD_Genetic_Distance/1000_genomes_20140502_plus_2-SNP-chip_plus_Mayo-Exome_plus_Genos-BWA-MEM-Exome_plus_Veritas_WGS
+
+##make sure you have .pop file with same name
+/opt/admixture_linux-1.3.0/admixture $PREFIX.bed 5 --supervised
