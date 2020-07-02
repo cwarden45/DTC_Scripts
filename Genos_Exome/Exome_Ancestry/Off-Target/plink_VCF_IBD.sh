065223f016af26ddc111b827201caa836bdcc12e
Charles Warden <cwarden45@gmail.com>
2020-07-02 15:09:36 -0700

    Add files via upload

---
+++ Genos_Exome/Exome_Ancestry/Off-Target/plink_VCF_IBD.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PREFIX=1000_genomes_20140502_plus_2-SNP-chip_plus_Genos-Off-Target-flank50000-ref286
+
+/opt/plink2 --make-bed --vcf $PREFIX.vcf --out $PREFIX
+
+/opt/plink2 --make-king-table -bfile $PREFIX --out $PREFIX
