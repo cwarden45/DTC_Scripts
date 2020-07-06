2cb241e8138d5d75d78ea33f3affec62723ce286
Charles Warden <cwarden45@gmail.com>
2020-07-06 15:12:03 -0700

    Add files via upload

--- Nebula/Gencove/GLIMPSE/rename_reference_chr.sh
+++ Nebula/Gencove/GLIMPSE/rename_reference_chr.sh
@@ -8,11 +8,11 @@ done >> chr_names.txt
 for i in 22
 do
 	echo $i
-	#/opt/bcftools-1.10.2/bcftools annotate --rename-chrs chr_names.txt  -O z ALL.chr$i\_GRCh38.genotypes.20170504.vcf.gz > ALL.chr$i\_GRCh38.genotypes.20170504.rename.vcf.gz
-	#/opt/bcftools-1.10.2/bcftools index -f ALL.chr$i\_GRCh38.genotypes.20170504.rename.vcf.gz
+	/opt/bcftools-1.10.2/bcftools annotate --rename-chrs chr_names.txt  -O z ALL.chr$i\_GRCh38.genotypes.20170504.vcf.gz > ALL.chr$i\_GRCh38.genotypes.20170504.rename.vcf.gz
+	/opt/bcftools-1.10.2/bcftools index -f ALL.chr$i\_GRCh38.genotypes.20170504.rename.vcf.gz
 	
-	#/opt/bcftools-1.10.2/bcftools view -G -m 2 -M 2 -v snps ALL.chr$i\_GRCh38.genotypes.20170504.rename.vcf.gz -Oz -o ALL.chr$i\_GRCh38_sites.20170504.rename.vcf.gz
-	#/opt/bcftools-1.10.2/bcftools index -f ALL.chr$i\_GRCh38_sites.20170504.rename.vcf.gz
+	/opt/bcftools-1.10.2/bcftools view -G -m 2 -M 2 -v snps ALL.chr$i\_GRCh38.genotypes.20170504.rename.vcf.gz -Oz -o ALL.chr$i\_GRCh38_sites.20170504.rename.vcf.gz
+	/opt/bcftools-1.10.2/bcftools index -f ALL.chr$i\_GRCh38_sites.20170504.rename.vcf.gz
 
 	/opt/bcftools-1.10.2/bcftools query -f'%CHROM\t%POS\t%REF,%ALT\n' ALL.chr$i\_GRCh38_sites.20170504.rename.vcf.gz | bgzip -c > ALL.chr$i\_GRCh38_sites.20170504.rename.tsv.gz
 	tabix -s1 -b2 -e2 ALL.chr$i\_GRCh38_sites.20170504.rename.tsv.gz
