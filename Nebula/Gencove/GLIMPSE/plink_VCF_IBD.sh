e3e39053e1fe28022a5b744120d9fe8b63f46a77
Charles Warden <cwarden45@gmail.com>
2020-07-26 13:45:27 -0700

    Add files via upload

---
+++ Nebula/Gencove/GLIMPSE/plink_VCF_IBD.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+PREFIX=1000_genomes_20140502_plus_2-SNP-chip_plus_Nebula-GLIMPSE-down10
+
+/opt/plink2 --make-bed --vcf $PREFIX.vcf --out $PREFIX
+
+/opt/plink2 --make-king-table -bfile $PREFIX --out $PREFIX
