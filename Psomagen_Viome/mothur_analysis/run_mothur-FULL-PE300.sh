87c7c27e83b1d5b0acb7eca3ac4ddb84b382d2aa
Charles Warden <cwarden45@gmail.com>
2022-04-30 10:37:34 -0700

    Add files via upload

---
+++ Psomagen_Viome/mothur_analysis/run_mothur-FULL-PE300.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BATCHFILE=16S_2021-SILVA-FULL-PE300.batch
+SUBFOLDER=.
+export REF=/home/cwarden/Ref/mothur/silva.nr_v132/silva.nr_v132
+export CONF=80
+
+#based upon https://mothur.org/wiki/miseq_sop/
+#general batch mode instructions: https://mothur.org/wiki/batch_mode/
+export DIR=$(pwd)
+export SUBFOLDER=$SUBFOLDER
+export PREFIX=16S_2021
+export PROC=4
+export MAXLENGTH=500
+
+##mothur v.1.44.3
+/opt/mothur/mothur \"$BATCHFILE\"
