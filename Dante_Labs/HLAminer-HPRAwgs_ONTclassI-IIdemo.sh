577028b09ce984e896044fbc184667f66885ddcf
Charles Warden <cwarden45@gmail.com>
2023-07-28 20:09:56 -0700

    Update HLAminer-HPRAwgs_ONTclassI-IIdemo.sh

--- Dante_Labs/HLAminer-HPRAwgs_ONTclassI-IIdemo.sh
+++ Dante_Labs/HLAminer-HPRAwgs_ONTclassI-IIdemo.sh
@@ -15,6 +15,6 @@ echo "Predicting HLA..."
 /opt/HLAminer-1.4/HLAminer_v1.4/bin/HLAminer.pl -a aln.sam -h $REF2 -q 1 -i 1 -p /opt/HLAminer-1.4/HLAminer_v1.4/database/hla_nom_p.txt -s 500
 echo "Additional files for samtools idxstats summary"
 /opt/samtools/samtools view -b aln.sam > aln.bam
-java -Xmx8g -jar /opt/picard-v2.21.9.jar AddOrReplaceReadGroups I=aln.bam O=rg.bam SO=coordinate RGID=1 RGLB=WGA RGPL=Illumina RGPU=SequencingCom RGSM=realign
+java -Xmx8g -jar /opt/picard-v2.21.9.jar AddOrReplaceReadGroups I=aln.bam O=rg.bam SO=coordinate RGID=1 RGLB=HiFi RGPL=PacBio RGPU=DanteLabs RGSM=realign
 /opt/samtools/samtools index rg.bam
 /opt/samtools/samtools idxstats rg.bam > idxstats.txt
