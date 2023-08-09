53176fcb73995f477d5981a96ec762a8d5787be3
Charles Warden <cwarden45@gmail.com>
2023-08-08 20:40:45 -0700

    Add files via upload

---
+++ Sequencing.com/run_HISAT-genotype-python3-v2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+R1=../CharlesWarden-NG1J8B7TDM-30x-WGS-Sequencing_com-10-13-21.1.fq.gz
+R2=../CharlesWarden-NG1J8B7TDM-30x-WGS-Sequencing_com-10-13-21.2.fq.gz
+THREADS=4
+OUTDIR=Sequencing.com_HISAT-genotype
+
+#reference saved under /home/cwarden/Ref/hisat-genotype/indicies/hisatgenotype_db
+
+export PATH=/opt/samtools:/opt/hisat2-2.2.1:$PATH
+export PYTHONPATH="$PYTHONPATH:/home/cwarden/Ref/hisat-genotype/hisatgenotype_modules"
+
+python3 /home/cwarden/Ref/hisat-genotype/hisatgenotype --base hla --locus-list A,B,C,DRB1,DQA1,DQB1 -1 $R1 -2 $R2 -p $THREADS --out-dir $OUTDIR
